f06d62218fba67c2b3c149f2031c48d8c22bb60c
elpsycongru <3261113647@qq.com>
2023-07-22 17:36:42 +0800

    Add attribute

---
+++ Assets/Attribute.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attribute : MonoBehaviour
+{
+    public int rank,health,depth_max; // 等级，生命，最大下潜深度
+    public float moveSpeed; // 移动速度
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
